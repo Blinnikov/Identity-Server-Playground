2c056ab1cd4b140c89d3030ce48dfc05828130bb
Igor Blinnikov <igor.blinnikov@gmail.com>
2017-02-16 11:28:34 +0300

    feat: add empty custom user service

---
+++ AllInOneMVC/AllInOneMVC/UserService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services.Default;
+using IdentityServer3.Core.Services.InMemory;
+
+namespace AllInOneMVC {
+    public class UserService : InMemoryUserService {
+        public UserService(List<InMemoryUser> users) : base(users) {}
+
+        public override Task PreAuthenticateAsync(PreAuthenticationContext context) {
+            // context.SignInMessage.IdP = "windows";
+
+            return base.PreAuthenticateAsync(context);
+        }
+
+        public override Task AuthenticateExternalAsync(ExternalAuthenticationContext context) {
+            return base.AuthenticateExternalAsync(context);
+        }
+
+        public override Task AuthenticateLocalAsync(LocalAuthenticationContext context) {
+            return base.AuthenticateLocalAsync(context);
+        }
+    }
+}
